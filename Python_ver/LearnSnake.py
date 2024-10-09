945a37451e07a2ea0ed4ef10dad1d88107a13e6d
Arhaan17 <17arhaan@gmail.com>
2024-10-09 18:03:02 +0530

    commit

--- Python_ver/LearnSnake.py
+++ Python_ver/LearnSnake.py
@@ -1,6 +1,9 @@
+# LearnSnake.py
+
 import random
 import numpy as np
 import pickle
+
 class LearnSnake:
     def __init__(self):         
         self.screen_width = 600
@@ -22,50 +25,54 @@ class LearnSnake:
         self.board[self.food_r][self.food_c] = 2
         self.survived = 0
         self.step()
+    
     def get_state(self):
         head_r, head_c = self.snake_coords[-1]
-        state = []
-        state.append(int(self.dir == "left"))
-        state.append(int(self.dir == "right"))
-        state.append(int(self.dir == "up"))
-        state.append(int(self.dir == "down"))
-        state.append(int(self.food_r < head_r))
-        state.append(int(self.food_r > head_r))
-        state.append(int(self.food_c < head_c))
-        state.append(int(self.food_c > head_c))
-        state.append(self.is_unsafe(head_r + 1, head_c))
-        state.append(self.is_unsafe(head_r - 1, head_c))
-        state.append(self.is_unsafe(head_r, head_c + 1))
-        state.append(self.is_unsafe(head_r, head_c - 1))
+        state = [
+            int(self.dir == "left"),
+            int(self.dir == "right"),
+            int(self.dir == "up"),
+            int(self.dir == "down"),
+            int(self.food_r < head_r),
+            int(self.food_r > head_r),
+            int(self.food_c < head_c),
+            int(self.food_c > head_c),
+            self.is_unsafe(head_r + 1, head_c),
+            self.is_unsafe(head_r - 1, head_c),
+            self.is_unsafe(head_r, head_c + 1),
+            self.is_unsafe(head_r, head_c - 1)
+        ]
         return tuple(state)
+    
     def is_unsafe(self, r, c):
         if self.valid_index(r, c):
-          if self.board[r][c] == 1:
-              return 1
-          return 0
-        else:
-          return 1
-    def get_dist(self, r1, c1, r2, c2):
-        return ((r2 - r1) ** 2 + (c2 - c1) ** 2) ** 0.5
+            return 1 if self.board[r][c] == 1 else 0
+        return 1
+
     def valid_index(self, r, c):
         return 0 <= r < len(self.board) and 0 <= c < len(self.board[0])
+
     def coords_to_index(self, x, y):
-        r = int(y // 10)
-        c = int(x // 10)
+        r = int(y // self.snake_size)
+        c = int(x // self.snake_size)
         return (r, c)
+
     def generate_food(self):
         food_c = int(round(random.randrange(0, self.screen_width - self.snake_size) / 10.0))
         food_r = int(round(random.randrange(0, self.screen_height - self.snake_size) / 10.0))
         if self.board[food_r][food_c] != 0:
-            food_r, food_c = self.generate_food()
+            return self.generate_food()
         return food_r, food_c
+
     def game_over(self):
         return self.game_close
+
     def step(self, action="None"):
         if action == "None":
             action = random.choice(["left", "right", "up", "down"])
         else:
             action = ["left", "right", "up", "down"][action]
+
         reward = 0
         if action == "left" and (self.dir != "right" or self.snake_length == 1):
             self.c_change = -1
@@ -83,40 +90,45 @@ class LearnSnake:
             self.r_change = 1
             self.c_change = 0
             self.dir = "down"
+
+        # Check boundaries
         if self.c1 >= self.screen_width // self.snake_size or self.c1 < 0 or self.r1 >= self.screen_height // self.snake_size or self.r1 < 0:
             self.game_close = True
+
         self.c1 += self.c_change
         self.r1 += self.r_change
         self.snake_coords.append((self.r1, self.c1))
+
         if self.valid_index(self.r1, self.c1):
             self.board[self.r1][self.c1] = 1
+        
         if len(self.snake_coords) > self.snake_length:
             rd, cd = self.snake_coords[0]
             del self.snake_coords[0]
             if self.valid_index(rd, cd):
                 self.board[rd][cd] = 0
+        
         for r, c in self.snake_coords[:-1]:
             if r == self.r1 and c == self.c1:
                 self.game_close = True
+        
         if self.c1 == self.food_c and self.r1 == self.food_r:
             self.food_r, self.food_c = self.generate_food()
             self.board[self.food_r][self.food_c] = 2
             self.snake_length += 1
-            reward = 1 # Khana
-        else:
-            rh1, ch1 = self.snake_coords[-1]
-            if len(self.snake_coords) == 1:
-                rh2, ch2 = rh1, ch1
-            else:
-                rh2, ch2 = self.snake_coords[-1]
+            reward = 1  # Food eaten
+
         if self.game_close:
-            reward = -10  # Dead
+            reward = -10  # Game over
+
         self.survived += 1    
         return self.get_state(), reward, self.game_close
+
     def run_game(self, episode):
         filename = f"Q_table_results/{episode}.pickle"
         with open(filename, 'rb') as file:
             table = pickle.load(file)
+
         current_length = 2
         steps_unchanged = 0
         while not self.game_over():
@@ -130,4 +142,5 @@ class LearnSnake:
                 current_length = self.snake_length
             else:
                 steps_unchanged += 1
+        
         return self.snake_length
--- Python_ver/SnakePygame.py
+++ Python_ver/SnakePygame.py
@@ -1,405 +1,66 @@
-# import random
-# import numpy as np
-# import pygame
-# import pickle
-# import time
-# class Color:
-#     def __init__(self):
-#         self.white = (255, 255, 255)
-#         self.black = (0, 0, 0)
-#         self.neon_red = (255, 0, 0)
-#         self.cyan = (0, 255, 255)
-#         self.blue = (0, 0, 255)
-# class SnakeGame:
-#     def __init__(self):
-#         self.show_episode = True
-#         self.episode = None
-#         self.scale = 2
-#         self.w = int(600 * self.scale)
-#         self.h = int(400 * self.scale)
-#         self.pad = int(30 * self.scale)
-#         self.sn_size = int(10 * self.scale)
-#         self.fd_size = int(10 * self.scale)
-#         self.spd = 20
-#         self.sn_coords = []
-#         self.sn_len = 1
-#         self.dir = "right"
-#         self.board = np.zeros((self.h // self.sn_size, self.w // self.sn_size))
-#         self.game_close = False
-#         self.x = self.w / 2
-#         self.y = self.h / 2 + self.pad
-#         self.r, self.c = self.coord_to_idx(self.x, self.y)
-#         self.board[self.r][self.c] = 1
-#         self.col_chg = 1
-#         self.row_chg = 0
-#         self.fd_r, self.fd_c = self.mk_food()
-#         self.board[self.fd_r][self.fd_c] = 2
-#         self.surv = 0
-#         pygame.init()
-#         self.color = Color()
-#         self.screen = pygame.display.set_mode((self.w, self.h + self.pad))
-#         self.clock = pygame.time.Clock()
-#         self.font = pygame.font.SysFont("bahnschrift", int(18 * self.scale))
-#         self.last_dir = None
-#         self.step()
-#     def show_score(self, score):
-#         text = self.font.render(f"Score: {score}", True, self.color.white)
-#         self.screen.blit(text, [500 * self.scale, 10])
-#     def draw_snake(self):
-#         for i in range(len(self.sn_coords) - 1, -1, -1):
-#             row, col = self.sn_coords[i]
-#             x, y = self.idx_to_coord(row, col)
-#             if i == len(self.sn_coords) - 1:
-#                 pygame.draw.rect(self.screen, self.color.blue, [x, y, self.sn_size, self.sn_size])  # Head
-#             else:
-#                 pygame.draw.rect(self.screen, self.color.cyan, [x, y, self.sn_size, self.sn_size])  # Body
-#     def game_end_msg(self):
-#         msg = self.font.render("Game over!", True, self.color.neon_red)
-#         self.screen.blit(msg, [2 * self.w / 5, 2 * self.h / 5 + self.pad])
-#     def is_unsafe(self, row, col):
-#         if self.valid_idx(row, col):
-#             if self.board[row][col] == 1:
-#                 return 1
-#             return 0
-#         else:
-#             return 1
-#     def get_state(self):
-#         hr, hc = self.sn_coords[-1]
-#         state = []
-#         state.append(int(self.dir == "left"))
-#         state.append(int(self.dir == "right"))
-#         state.append(int(self.dir == "up"))
-#         state.append(int(self.dir == "down"))
-#         state.append(int(self.fd_r < hr))
-#         state.append(int(self.fd_r > hr))
-#         state.append(int(self.fd_c < hc))
-#         state.append(int(self.fd_c > hc))
-#         state.append(self.is_unsafe(hr + 1, hc))
-#         state.append(self.is_unsafe(hr - 1, hc))
-#         state.append(self.is_unsafe(hr, hc + 1))
-#         state.append(self.is_unsafe(hr, hc - 1))
-#         return tuple(state)
-#     def valid_idx(self, row, col):
-#         return 0 <= row < len(self.board) and 0 <= col < len(self.board[0])
-#     def idx_to_coord(self, row, col):
-#         x = col * self.sn_size
-#         y = row * self.sn_size + self.pad
-#         return (x, y)
-#     def coord_to_idx(self, x, y):
-#         row = int((y - self.pad) // self.sn_size)
-#         col = int(x // self.sn_size)
-#         return (row, col)
-#     def mk_food(self):
-#         col = int(round(random.randrange(0, self.w - self.fd_size) / self.fd_size))
-#         row = int(round(random.randrange(0, self.h - self.fd_size) / self.fd_size))
-#         if self.board[row][col] != 0:
-#             row, col = self.mk_food()
-#         return row, col
-#     def game_over(self):
-#         return self.game_close
-#     def step(self, action="None"):
-#         if action == "None":
-#             action = random.choice(["left", "right", "up", "down"])
-#         else:
-#             action = ["left", "right", "up", "down"][action]
-#         for event in pygame.event.get():
-#             pass
-#         self.last_dir = self.dir
-#         if action == "left" and (self.dir != "right" or self.sn_len == 1):
-#             self.col_chg = -1
-#             self.row_chg = 0
-#             self.dir = "left"
-#         elif action == "right" and (self.dir != "left" or self.sn_len == 1):
-#             self.col_chg = 1
-#             self.row_chg = 0
-#             self.dir = "right"
-#         elif action == "up" and (self.dir != "down" or self.sn_len == 1):
-#             self.row_chg = -1
-#             self.col_chg = 0
-#             self.dir = "up"
-#         elif action == "down" and (self.dir != "up" or self.sn_len == 1):
-#             self.row_chg = 1
-#             self.col_chg = 0
-#             self.dir = "down"
-#         if self.c >= self.w // self.sn_size or self.c < 0 or self.r >= self.h // self.sn_size or self.r < 0:
-#             self.game_close = True
-#         self.c += self.col_chg
-#         self.r += self.row_chg
-#         self.screen.fill(self.color.black)
-#         pygame.draw.rect(self.screen, self.color.white, (0, self.pad, self.w, self.h), 1)
-#         fd_x, fd_y = self.idx_to_coord(self.fd_r, self.fd_c)
-#         pygame.draw.rect(self.screen, self.color.neon_red, [fd_x, fd_y, self.fd_size, self.fd_size])
-#         self.sn_coords.append((self.r, self.c))
-#         if self.valid_idx(self.r, self.c):
-#             self.board[self.r][self.c] = 1
-#         if len(self.sn_coords) > self.sn_len:
-#             r_del, c_del = self.sn_coords[0]
-#             del self.sn_coords[0]
-#             if self.valid_idx(r_del, c_del):
-#                 self.board[r_del][c_del] = 0
-#         for r, c in self.sn_coords[:-1]:
-#             if r == self.r and c == self.c:
-#                 self.game_close = True
-#         self.draw_snake()
-#         self.show_score(self.sn_len - 1)
-#         pygame.display.update()
-#         if self.c == self.fd_c and self.r == self.fd_r:
-#             self.fd_r, self.fd_c = self.mk_food()
-#             self.board[self.fd_r][self.fd_c] = 2
-#             self.sn_len += 1
-#         self.surv += 1
-#     def run(self, ep):
-#         self.show_episode = True
-#         self.episode = ep
-#         pygame.display.update()
-#         fname = f"Q_table_results/{ep}.pickle"
-#         with open(fname, 'rb') as file:
-#             table = pickle.load(file)
-#         time.sleep(5)
-#         cur_len = 2
-#         unchanged_steps = 0
-#         while not self.game_over():
-#             if self.sn_len != cur_len:
-#                 unchanged_steps = 0
-#                 cur_len = self.sn_len
-#             else:
-#                 unchanged_steps += 1
-#             state = self.get_state()
-#             action = np.argmax(table[state])
-#             if unchanged_steps == 1000:
-#                 break
-#             self.step(action)
-#             self.clock.tick(self.spd)
-#         if self.game_over():
-#             self.screen.fill(self.color.black)
-#             pygame.draw.rect(self.screen, self.color.white, (0, self.pad, self.w, self.h), 1)
-#             self.game_end_msg()
-#             self.show_score(self.sn_len - 1)
-#             pygame.display.update()
-#             time.sleep(2)
-#         pygame.quit()
-# if __name__ == "__main__":
-#     game = SnakeGame()
-#     try :
-#         game.run(100000)
-#     except KeyboardInterrupt:
-#         print("\t\t\t\t\t\t\t!!!Done Already!!!")
+# SnakePygame.py
 
-import random
-import numpy as np
 import pygame
+from SnakeQAgent import SnakeQAgent
+from LearnSnake import LearnSnake
+import numpy as np
 import pickle
-import time
 
-class Color:
-    def __init__(self):
-        self.white = (255, 255, 255)
-        self.off_white = (240, 240, 240)  # Off-white color
-        self.black = (0, 0, 0)
-        self.neon_red = (255, 0, 0)
-        self.blue = (0, 0, 255)
+class SnakeGameVisualizer:
+    def __init__(self, q_table):
+        self.q_table = q_table
+        self.env = LearnSnake()
+        self.screen_width = self.env.screen_width
+        self.screen_height = self.env.screen_height
+        self.snake_size = self.env.snake_size
+        self.game_over = False
 
-class SnakeGame:
-    def __init__(self):
-        self.show_episode = True
-        self.episode = None
-        self.scale = 2
-        self.w = int(600 * self.scale)
-        self.h = int(400 * self.scale)
-        self.pad = int(30 * self.scale)
-        self.sn_size = int(10 * self.scale)
-        self.fd_size = int(10 * self.scale)
-        self.spd = 20
-        self.sn_coords = []
-        self.sn_len = 1
-        self.dir = "right"
-        self.board = np.zeros((self.h // self.sn_size, self.w // self.sn_size))
-        self.game_close = False
-        self.x = self.w / 2
-        self.y = self.h / 2 + self.pad
-        self.r, self.c = self.coord_to_idx(self.x, self.y)
-        self.board[self.r][self.c] = 1
-        self.col_chg = 1
-        self.row_chg = 0
-        self.fd_r, self.fd_c = self.mk_food()
-        self.board[self.fd_r][self.fd_c] = 2
-        self.surv = 0
+    def draw_elements(self):
+        # Initialize Pygame
         pygame.init()
-        self.color = Color()
-        self.screen = pygame.display.set_mode((self.w, self.h + self.pad))
-        self.clock = pygame.time.Clock()
-        self.font = pygame.font.SysFont("bahnschrift", int(18 * self.scale))
-        self.last_dir = None
-        
-        # Load images
-        self.head_image = pygame.image.load("snake_head.png").convert_alpha()
-        self.food_image = pygame.image.load("food.png").convert_alpha()
-        self.head_image = pygame.transform.scale(self.head_image, (self.sn_size, self.sn_size))
-        self.food_image = pygame.transform.scale(self.food_image, (self.fd_size, self.fd_size))
-        
-        self.step()
+        screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("Snake Game")
 
-    def show_score(self, score):
-        text = self.font.render(f"Score: {score}", True, self.color.white)
-        self.screen.blit(text, [500 * self.scale, 10])
+        while not self.game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.game_over = True
 
-    def draw_snake(self):
-        for i in range(len(self.sn_coords) - 1, -1, -1):
-            row, col = self.sn_coords[i]
-            x, y = self.idx_to_coord(row, col)
-            if i == len(self.sn_coords) - 1:
-                self.screen.blit(self.head_image, (x, y))  # Head
-            else:
-                pygame.draw.rect(self.screen, self.color.off_white, [x, y, self.sn_size, self.sn_size])  # Body
+            # Get the current state
+            state = self.env.get_state()
+            action = np.argmax(self.q_table[state])  # Choose the best action based on Q-table
+            new_state, reward, self.env.game_close = self.env.step(action)
 
-    def game_end_msg(self):
-        msg = self.font.render("Game over!", True, self.color.neon_red)
-        self.screen.blit(msg, [2 * self.w / 5, 2 * self.h / 5 + self.pad])
+            # Draw the game elements
+            screen.fill((0, 0, 0))  # Clear the screen
 
-    def is_unsafe(self, row, col):
-        if self.valid_idx(row, col):
-            if self.board[row][col] == 1:
-                return 1
-            return 0
-        else:
-            return 1
+            # Draw the snake
+            for r, c in self.env.snake_coords:
+                pygame.draw.rect(screen, (0, 255, 0), [c * self.snake_size, r * self.snake_size, self.snake_size, self.snake_size])
 
-    def get_state(self):
-        hr, hc = self.sn_coords[-1]
-        state = []
-        state.append(int(self.dir == "left"))
-        state.append(int(self.dir == "right"))
-        state.append(int(self.dir == "up"))
-        state.append(int(self.dir == "down"))
-        state.append(int(self.fd_r < hr))
-        state.append(int(self.fd_r > hr))
-        state.append(int(self.fd_c < hc))
-        state.append(int(self.fd_c > hc))
-        state.append(self.is_unsafe(hr + 1, hc))
-        state.append(self.is_unsafe(hr - 1, hc))
-        state.append(self.is_unsafe(hr, hc + 1))
-        state.append(self.is_unsafe(hr, hc - 1))
-        return tuple(state)
+            # Draw the food
+            pygame.draw.rect(screen, (255, 0, 0), [self.env.food_c * self.snake_size, self.env.food_r * self.snake_size, self.snake_size, self.snake_size])
 
-    def valid_idx(self, row, col):
-        return 0 <= row < len(self.board) and 0 <= col < len(self.board[0])
+            pygame.display.update()  # Update the display
+            pygame.time.delay(100)  # Delay to control the speed of the game
 
-    def idx_to_coord(self, row, col):
-        x = col * self.sn_size
-        y = row * self.sn_size + self.pad
-        return (x, y)
+            if self.env.game_close:
+                print(f"Game Over! Snake Length: {self.env.snake_length}")
+                self.game_over = True
 
-    def coord_to_idx(self, x, y):
-        row = int((y - self.pad) // self.sn_size)
-        col = int(x // self.sn_size)
-        return (row, col)
+def main():
+    # Training Phase
+    episodes = 5000  # Number of training episodes
+    agent = SnakeQAgent(episodes)
+    agent.train()  # Start training the agent
 
-    def mk_food(self):
-        col = int(round(random.randrange(0, self.w - self.fd_size) / self.fd_size))
-        row = int(round(random.randrange(0, self.h - self.fd_size) / self.fd_size))
-        if self.board[row][col] != 0:
-            row, col = self.mk_food()
-        return row, col
+    # Load the trained Q-table for testing
+    with open('Q_table_results/5000.pickle', 'rb') as file:
+        q_table = pickle.load(file)
 
-    def game_over(self):
-        return self.game_close
-
-    def step(self, action="None"):
-        if action == "None":
-            action = random.choice(["left", "right", "up", "down"])
-        else:
-            action = ["left", "right", "up", "down"][action]
-        
-        for event in pygame.event.get():
-            pass
-        
-        self.last_dir = self.dir
-        if action == "left" and (self.dir != "right" or self.sn_len == 1):
-            self.col_chg = -1
-            self.row_chg = 0
-            self.dir = "left"
-        elif action == "right" and (self.dir != "left" or self.sn_len == 1):
-            self.col_chg = 1
-            self.row_chg = 0
-            self.dir = "right"
-        elif action == "up" and (self.dir != "down" or self.sn_len == 1):
-            self.row_chg = -1
-            self.col_chg = 0
-            self.dir = "up"
-        elif action == "down" and (self.dir != "up" or self.sn_len == 1):
-            self.row_chg = 1
-            self.col_chg = 0
-            self.dir = "down"
-        
-        if self.c >= self.w // self.sn_size or self.c < 0 or self.r >= self.h // self.sn_size or self.r < 0:
-            self.game_close = True
-        
-        self.c += self.col_chg
-        self.r += self.row_chg
-        self.screen.fill(self.color.black)
-        pygame.draw.rect(self.screen, self.color.white, (0, self.pad, self.w, self.h), 1)
-        
-        fd_x, fd_y = self.idx_to_coord(self.fd_r, self.fd_c)
-        self.screen.blit(self.food_image, [fd_x, fd_y])  # Food
-        
-        self.sn_coords.append((self.r, self.c))
-        if self.valid_idx(self.r, self.c):
-            self.board[self.r][self.c] = 1
-        if len(self.sn_coords) > self.sn_len:
-            r_del, c_del = self.sn_coords[0]
-            del self.sn_coords[0]
-            if self.valid_idx(r_del, c_del):
-                self.board[r_del][c_del] = 0
-        
-        for r, c in self.sn_coords[:-1]:
-            if r == self.r and c == self.c:
-                self.game_close = True
-        
-        self.draw_snake()
-        self.show_score(self.sn_len - 1)
-        pygame.display.update()
-        
-        if self.c == self.fd_c and self.r == self.fd_r:
-            self.fd_r, self.fd_c = self.mk_food()
-            self.board[self.fd_r][self.fd_c] = 2
-            self.sn_len += 1
-        
-        self.surv += 1
-
-    def run(self, ep):
-        self.show_episode = True
-        self.episode = ep
-        pygame.display.update()
-        fname = f"Q_table_results/{ep}.pickle"
-        with open(fname, 'rb') as file:
-            table = pickle.load(file)
-        time.sleep(5)
-        cur_len = 2
-        unchanged_steps = 0
-        while not self.game_over():
-            if self.sn_len != cur_len:
-                unchanged_steps = 0
-                cur_len = self.sn_len
-            else:
-                unchanged_steps += 1
-            state = self.get_state()
-            action = np.argmax(table[state])
-            if unchanged_steps == 1000:
-                break
-            self.step(action)
-            self.clock.tick(self.spd)
-        if self.game_over():
-            self.screen.fill(self.color.black)
-            pygame.draw.rect(self.screen, self.color.white, (0, self.pad, self.w, self.h), 1)
-            self.game_end_msg()
-            self.show_score(self.sn_len - 1)
-            pygame.display.update()
-            time.sleep(2)
-        pygame.quit()
+    # Testing Phase
+    visualizer = SnakeGameVisualizer(q_table)
+    visualizer.draw_elements()  # Start the visualization of the trained agent
 
 if __name__ == "__main__":
-    game = SnakeGame()
-    try:
-        game.run(100000)
-    except KeyboardInterrupt:
-        print("\t\t\t\t\t\t\t!!!Done Already!!!")
+    main()
